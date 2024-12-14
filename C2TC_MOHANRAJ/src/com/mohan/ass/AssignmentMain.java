4d44a75600dd944c137fb8444f5da718d86f8285
Dell <Dell@192.168.31.79>
2024-12-14 20:12:53 +0530

    fist commit

---
+++ C2TC_MOHANRAJ/src/com/mohan/ass/AssignmentMain.java
@@ -0,0 +1,10 @@
+package com.mohan.ass;
+import com.mohan.ass.*;
+public class AssignmentMain {
+public static void main(String[] args) {
+Manager manager = new Manager("John", 101, 90000, "Sales");
+Developer developer = new Developer("Alice", 102, 80000, "Java");
+EmployeeUtilities.printEmployeeDetails(manager);
+EmployeeUtilities.printEmployeeDetails(developer);
+}
+}
---
+++ C2TC_MOHANRAJ/src/com/mohan/ass/EmployeeUtilities.java
@@ -0,0 +1,17 @@
+package com.mohan.ass;
+import com.mohan.ass.*;
+public class EmployeeUtilities {
+public static void printEmployeeDetails(Employee employee) {
+System.out.println("Employee Name: " + employee.getName());
+System.out.println("Employee ID: " + employee.getEmployeeId());
+System.out.println("Salary: " + employee.getSalary());
+if (employee instanceof Manager) {
+Manager manager = (Manager) employee;
+System.out.println("Department: " + manager.getDepartment());
+} else if (employee instanceof Developer) {
+Developer developer = (Developer) employee;
+System.out.println("Programming Language: " + developer.getProgrammingLanguage());
+}
+System.out.println();
+}
+}
